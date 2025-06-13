e2514b1ffdf1681b61e92fc0ab97ccadfdbd44c2
sakamw <shanmwangi2020@gmail.com>
2025-06-13 19:07:52 +0300

    add Create and Get all Users

--- index.js
+++ index.js
@@ -6,7 +6,43 @@ const client = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-// Listener
+app.get("/", (_req, res) => {
+  res.send("<h1> Welcome to Blog API </h1>");
+});
+
+// Create new User
+app.post("/users", async (req, res) => {
+  try {
+    const { firstName, lastName, emailAddress, username } = req.body;
+    const newUser = await client.users.create({
+      data: { firstName, lastName, emailAddress, username },
+    });
+    res.status(201).json(newUser);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+// Get all Users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await client.users.findMany({
+      select: {
+        firstName: true,
+        lastName: true,
+        emailAddress: true,
+        username: true,
+      },
+    });
+    res.status(200).json(users);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: "Failed to Fetch Users" });
+  }
+});
+
+// Port Listener
 const port = process.env.PORT || 4500;
 app.listen(port, () => {
   console.log(`App listening at port ${port}`);
